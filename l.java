795d3c1daf6e1db927231ff0df8019bc8194ab17
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2022-04-20 10:14:03 +0530

    Longest substring in string of digits which can be divided into two equal parts such that sum of the two parts is same

---
+++ l.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class l{
+    static int findlength(String str,int n)
+    {
+        int ans=0;
+        for(int i=0;i<=n-2;i++)
+        {
+            int l=i,r=i+1;
+            int lsum=0,rsum=0;
+            while(r<n && l>=0)
+            {
+                lsum+=str.charAt(l)-'0';
+                rsum+=str.charAt(r)-'0';
+                if(lsum==rsum)
+                {
+                    ans=Math.max(ans,r-l+1);
+                }
+                r++;
+                l--;
+            }
+        }
+        return ans;
+    }
+    public static void main(String args[])
+    {
+        String str="123123";
+        int n=str.length();
+        System.out.println(findlength(str,n));
+    }
+
+}
---
+++ l3.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class l3 {
+    static int findlong(String str,int n)
+    {
+        int ans=0;
+        for(int i=0;i<=n-2;i++)
+        {
+            int l=i,r=i+1;
+            int lsum=0,rsum=0;
+            while(r<n && l>=0)
+            {
+                lsum+=str.charAt(l)-'0';
+                rsum+=str.charAt(r)-'0';
+                if(lsum==rsum)
+                {
+                    ans=Math.max(ans,r-l+1);
+                }
+                l--;
+                r++;
+
+            }
+        }
+        return ans;
+    }
+    public static void main(String args[])
+    {
+        String str="123123";
+        int n=str.length();
+        System.out.println(findlong(str,n));
+    }
+}
